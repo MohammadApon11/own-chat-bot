d60bd8d39041e855f5843d146ca94c8752bf9524
moahammadApon <mohammadapon11.com>
2024-03-30 20:25:54 +0600

    signup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import Login from "./pages/login/Login";
-import Signup from "./pages/signup/Signup";
 import Home from "./pages/home/Home";
+import Signup from "./pages/signup/Signup";
+import Login from "./pages/login/Login";
+import { Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Toaster />
     </div>
   );
 };
