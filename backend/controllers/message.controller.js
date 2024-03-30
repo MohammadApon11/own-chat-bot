60dc16627bbffe8e8680c75bdb4dcb217897d7ba
moahammadApon <mohammadapon11.com>
2024-03-31 00:31:56 +0600

    all are done

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async (req, res) => {
   try {
@@ -28,9 +29,13 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
-    // SOCKET IO INTERGRATE
-
     await Promise.all([conversation.save(), newMessage.save()]);
+
+    // SOCKET IO INTERGRATE
+    const receiverSocketId = getReceiverSocketId(receiverId);
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(200).send(newMessage);
   } catch (error) {
